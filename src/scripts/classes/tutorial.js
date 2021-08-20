5764b9d7fa85b8b7d07e49aa66dd65fad4d60242
theonewei <wei.n.isaac@gmail.com>
2021-08-20 11:18:33 -0400

    finish tutorial

--- src/scripts/classes/tutorial.js
+++ src/scripts/classes/tutorial.js
@@ -5,14 +5,26 @@ class Tutorial{
   }
 
   render(){
-    const box = document.createElement('div')
-    box.innerText = 'hello'
+    const box = document.createElement('p')
     box.id = 'tutorial'
+
+    const title = document.createElement('div')
+    title.id = 'title'
+    title.innerText = 'Pathfinding Algorithm Visualizer:'
+
+    const subtitle = document.createElement('div')
+    subtitle.id = 'subtitle'
+    subtitle.innerText = "Pathfinding algorithms are used to find path's between two given points on a graph. Algorithms differ in their speed at finding a path and also how efficient the path they find is"
+
     const x = document.createElement('div')
     x.innerText = 'x'
     x.id = 'close-btn'
+
     x.onclick = this._toggleVisiblity
-    box.append(x)
+    const instructions = document.createElement('div')
+    instructions.innerText="1. Drag Start and End Nodes to where you want them to be\n \n 2. If you want some obstacles for the algorithm to navigate around, you can either click on nodes to turn them into walls or use the maze generator"
+
+    box.append(title,subtitle,x,instructions)
 
     const modal = document.createElement('div')
     modal.append(box)
